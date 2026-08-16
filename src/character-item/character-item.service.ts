7c6baae525f3c417f6be865e780ccf776a310071
luismutton <lfmutton@gmail.com>
2026-08-16 14:37:16 -0300

    feat(char-item): criado char-item service

--- src/character-item/character-item.service.ts
+++ src/character-item/character-item.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CharacterItem } from './character-item.entity';
@@ -12,10 +12,43 @@ export class CharacterItemService {
     private itemsService: ItemsService,
   ) {}
 
-  create(idChar: number, body: Partial<CharacterItem>) {
+  async create(idChar: number, body: Partial<CharacterItem>) {
+    const item = await this.itemsService.findOne(body.itemId!);
+
+    if (!item) throw new NotFoundException('Item not found');
+
     body.characterId = idChar;
+
     const characterItem = this.repo.create(body);
 
     return this.repo.save(characterItem);
+  }
+
+  async find(idChar: number, idItem: number) {
+    const item = await this.repo.findOne({
+      where: { itemId: idItem, characterId: idChar },
+    });
+
+    if (!item) {
+      throw new NotFoundException();
+    }
+
+    return item;
+  }
+
+  async update(attrs: Partial<CharacterItem>, idChar: number, idItem: number) {
+    const item = await this.find(idChar, idItem);
+
+    Object.assign(item, attrs);
+
+    return this.repo.save(item);
+  }
+
+  async delete(idChar: number, idItem: number) {
+    const item = await this.find(idChar, idItem);
+
+    this.repo.remove(item);
+
+    return this.repo.save(item);
   }
 }
